81d0f0f3abf1b3e37065b65a8b24ef0270c355e9
Oscar Chavez <oscar.a.chavez.jr@gmail.com>
2020-09-18 20:30:14 -0700

    added port variable

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get("/", function (req, res){
 
 
 
-app.listen(3000, function(){
-  console.log("Connected on port 3000!")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
