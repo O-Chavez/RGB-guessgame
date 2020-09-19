e4e9a61848d0ca8664af1ff20c1c1fcf2321f3a6
Oscar Chavez <oscar.a.chavez.jr@gmail.com>
2020-09-18 19:43:54 -0700

    refactored to nodejs

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+
+app = express();
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res){
+  res.render("Main")
+})
+
+
+
+
+app.listen(3000, function(){
+  console.log("Connected on port 3000!")
+});
